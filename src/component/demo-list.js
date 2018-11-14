8dda15b07eba7394479806a6a44b9e04947e47d6
Akigaze <1175520196@qq.com>
2018-11-14 19:21:09 +0800

    [Akigaze] add babel-preset-stage-1 for ES6 class property supporting arrow function

--- src/component/demo-list.js
+++ src/component/demo-list.js
@@ -6,13 +6,14 @@ export default class DemoList extends Component {
     constructor(props) {
         super(props);
     }
+    click = () => {
+        console.log("click me");
+    }
     render(){
+        let items = ["React", "Jasmine", "Enzyme", "Jamsine-Enzyme"];
         return(
             <ul className="list">
-                <li>React</li>
-                <li>Jamsine</li>
-                <li>Enzyme</li>
-                <li>Jamsine-Enzyme</li>
+                {items.map(item => <li key={item} onClick={this.click}>{item}</li>)}
             </ul>
         )
     }
