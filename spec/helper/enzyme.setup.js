011a3413661f19bd7ae7a6e0b84486bde00d99a0
Akigaze <1175520196@qq.com>
2018-12-05 21:01:42 +0800

    [Akigaze] import jest and jest-enzyme

--- spec/helper/enzyme.setup.js
+++ spec/helper/enzyme.setup.js
@@ -1,9 +1,4 @@
 import {configure} from "enzyme";
-import jasmineEnzyme from "jasmine-enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({adapter: new Adapter()});
-
-beforeEach(function() {
-  jasmineEnzyme();
-});
